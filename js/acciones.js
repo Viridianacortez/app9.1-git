ace1b89b773cb8581022ff1e754972657f176d33
Viridianacortez <cortez.diaz.3h@gmail.com>
2017-05-23 21:41:06 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+
 $(document).ready(function(e) {
     document.addEventListener("deviceready", onDeviceReady, false);
 	
@@ -27,13 +28,13 @@ function getPosition(){
 	
 	function onSuccess(position){
 		alert('Latitude:'       +position.coords.latitude    +'\n'+
-		'Longitude:'       +position.coords.longitude    +'\n'+
-		'Altitude:'       +position.coords.altitude    +'\n'+
-		'Accuracy:'       +position.coords.accuracy    +'\n'+
+		'Longitude:'       + position.coords.longitude    +'\n'+
+		'Altitude:'       + position.coords.altitude    +'\n'+
+		'Accuracy:'       + position.coords.accuracy    +'\n'+
 		'Altitude Accuracy:'       +position.coords.altitudeAccuracy   +'\n'+
-		'Heading:'       +position.coords.heading    +'\n'+
-		'Speed:'       +position.coords.speed    +'\n'+
-		'Timestamp:'       +position.timestamp    +'\n');
+		'Heading:'       + position.coords.heading    +'\n'+
+		'Speed:'       + position.coords.speed    +'\n'+
+		'Timestamp:'       + position.timestamp    +'\n');
 	};
 	
 	function onError(error){
@@ -60,11 +61,10 @@ function watchPosition(){
 		$('#aacuracy').html(position.coords.altitudeAccuracy);
 		$('#headingg').html(position.coords.heading);
 		$('#speed').html(position.coords.speed);
-		$('#timestamp').html(position.timestamp);
+		$('#timestamp').html(position.coords.timestamp);
 	};
 	
 	function onError(error){
 		alert('code:' + error.code  + '\n'+'message:'+error.message+'\n');
 	}
 }
-
